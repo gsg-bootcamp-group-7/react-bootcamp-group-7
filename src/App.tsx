0f8e9b3199bef9c845bc461936cf7406ecc43a8e
Anas Badran <ansaccts@gmail.com>
2026-01-04 15:06:46 +0200

    chore: setup mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "@mantine/core/styles.css";
+import { Button, MantineProvider } from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MantineProvider>
+        <h1>Hello, world</h1>
+        <Button
+          onClick={() => {
+            setCount((p) => ++p);
+          }}
+        >
+          click me {count}
+        </Button>
+      </MantineProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
