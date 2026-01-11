56e2f5832beef2eaa88b53e13e9da2cc7072ac76
Anas Badran <ansaccts@gmail.com>
2026-01-11 11:33:37 +0200

    feat: add simple view for products

--- src/modules/Products/views/index.tsx
+++ src/modules/Products/views/index.tsx
@@ -6,39 +6,51 @@ import {
   Image,
   SimpleGrid,
   Text,
+  Title,
 } from "@mantine/core";
 import { useGetAllProducts } from "../hooks/useGetAllProducts";
 
 export const Products = () => {
   const { products } = useGetAllProducts();
+  console.log(products);
   return (
-    <SimpleGrid>
-      {products.map((product) => (
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image
-              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-              height={160}
-              alt="Norway"
-            />
-          </Card.Section>
+    <>
+      <Title>Browse Products</Title>
 
-          <Group justify="space-between" mt="md" mb="xs">
-            <Text fw={500}>Norway Fjord Adventures</Text>
-            <Badge color="pink">On Sale</Badge>
-          </Group>
+      <SimpleGrid
+        cols={{
+          sm: 2,
+          md: 3,
+          lg: 4,
+        }}
+      >
+        {products.map((product) => (
+          <Card
+            key={product.id}
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
+          >
+            <Card.Section>
+              <Image src={product.image} height={160} alt="Norway" />
+            </Card.Section>
 
-          <Text size="sm" c="dimmed">
-            With Fjord Tours you can explore more of the magical fjord
-            landscapes with tours and activities on and around the fjords of
-            Norway
-          </Text>
+            <Group justify="space-between" mt="md" mb="xs">
+              <Text fw={500}>{product.name}</Text>
+              {product.isAvailable && <Badge color="pink">On Sale</Badge>}
+            </Group>
 
-          <Button color="blue" fullWidth mt="md" radius="md">
-            Book classic tour now
-          </Button>
-        </Card>
-      ))}
-    </SimpleGrid>
+            <Text size="sm" mb="md" c="dimmed">
+              {product.description}
+            </Text>
+
+            <Button color="blue" fullWidth mt="md" mt="auto" radius="md">
+              Order now
+            </Button>
+          </Card>
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
