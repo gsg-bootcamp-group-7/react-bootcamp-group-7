73ad7e0755e440d56be4068eb3deeaa463f1d2ec
Anas Badran <ansaccts@gmail.com>
2026-01-11 11:33:19 +0200

    feat: defined AppShell in root

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "@mantine/core/styles.css";
 import { Products } from "./modules/Products/views";
+import { AppShell, AppShellMain } from "@mantine/core";
 
 function App() {
   return (
     <>
+      <AppShell padding="md">
+        <AppShellMain>
+          <Products />
+        </AppShellMain>
+      </AppShell>
       <Products />
     </>
   );
